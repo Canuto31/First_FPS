863df9e25c393b6071200fc50565a7cd933065c0
Juan Moya <jcmt.personal.vg@gmail.com>
2023-05-25 18:06:04 -0500

    Project creation

---
+++ Source/First_FPS/First_FPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "First_FPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, First_FPS, "First_FPS" );
---
+++ Source/First_FPS/First_FPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "First_FPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRST_FPS_API AFirst_FPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
